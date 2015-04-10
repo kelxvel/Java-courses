1b7774551d27e5433a459ebec15e193630d20e75
kelxvel <kelxvel@gmail.com>
2015-04-10 21:31:33 +0300

    Refactoring task3

--- task3/Task3.java
+++ task3/Task3.java
@@ -1,88 +1,20 @@
 import java.io.*;
 import java.util.*;
 
-public class Task3 {
-    public static double[] readDoubleArray(File file) throws IOException {
-        try (Scanner fin = new Scanner(file)) {
-            List<Double> list = new ArrayList<>();
-
-            while (fin.hasNextDouble())
-                list.add(fin.nextDouble());
-
-            double[] array = new double[list.size()];
-            for (int i = 0; i < array.length; i++)
-                array[i] = list.get(i);
-            list.clear();
-
-            return array;
-        }
-    }
-
-    public static void main(String[] args) {
-        try {
-            if (args.length != 2)
-                throw new IOException("Usage: [sort type] [input file name]");
-
-            double[] array = readDoubleArray(new File(args[1]));
-
-            String sortType = args[0].toLowerCase();
-            Sortable sortable = null;
-            switch (sortType) {
-                case "quick":
-                    sortable = new QuickSortableArray(array);
-                    break;
-                case "bubble":
-                    sortable = new BubbleSortableArray(array);
-                    break;
-                case "shift":
-                    sortable = new ShiftSortableArray(array);
-                    break;
-                default:
-                    throw new IOException("Unresolved sort type \"" + sortType + "\"");
-            }
-            sortable.sort();
-
-            System.out.println(sortable);
-        }
-        catch (IOException e) {
-            System.err.println(e.getMessage());
-        }
-    }
+interface Sorter {
+    void sort(double[] array);
 }
 
-interface Sortable {
-    void sort();
-}
+class QuickSorter implements Sorter {
+    private QuickSorter() {}
 
-class SortableArray implements Sortable {
-    protected double[] array;
+    private static final QuickSorter INSTANCE = new QuickSorter();
 
-    public SortableArray(double[] array) {
-        this.array = array.clone();
+    public static QuickSorter getInstance() {
+        return INSTANCE;
     }
 
-    public void sort() {
-        Arrays.sort(array);
-    }
-
-    public void swap(int i, int j) {
-        double temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-
-    public String toString() {
-        return Arrays.toString(array);
-    }
-}
-
-class QuickSortableArray extends SortableArray {
-
-    public QuickSortableArray(double[] array) {
-        super(array);
-    }
-
-    public void sort() {
+    public void sort(double[] array) {
         quickSort(array, 0, array.length-1);
     }
 
@@ -90,43 +22,61 @@ class QuickSortableArray extends SortableArray {
         if (left >= right)
             return;
 
-        swap(left, (left + right) / 2);
+        swap(array, left, (left + right) / 2);
 
         int last = left;
         for (int i = left+1; i <= right; i++)
             if (array[i] < array[left])
-                swap(++last, i);
+                swap(array, ++last, i);
 
-        swap(left, last);
+        swap(array, left, last);
 
         quickSort(array, left, last-1);
         quickSort(array, last+1, right);
     }
-}
 
-class BubbleSortableArray extends SortableArray {
-
-    public BubbleSortableArray(double[] array) {
-        super(array);
-    }
-
-    public void sort() {
-	for (int k = array.length-1; k >=0; k--) {
-	    for (int i = 0; i < k; i++) {
-		if (array[i] > array[i+1])
-		   swap(i, i+1);
-	    }
-	}
+    public static void swap(double[] array, int i, int j) {
+        double temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 }
 
-class ShiftSortableArray extends SortableArray {
+class BubbleSorter implements Sorter {
+    private BubbleSorter() {}
 
-    public ShiftSortableArray(double[] array) {
-        super(array);
+    private static final BubbleSorter INSTANCE = new BubbleSorter();
+
+    public static BubbleSorter getInstance() {
+        return INSTANCE;
     }
 
-    public void sort() {
+    public void sort(double[] array) {
+        for (int k = array.length-1; k >=0; k--) {
+            for (int i = 0; i < k; i++) {
+                if (array[i] > array[i+1])
+                    swap(array, i, i+1);
+            }
+        }
+    }
+
+    public static void swap(double[] array, int i, int j) {
+        double temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
+class ShiftSorter implements Sorter {
+    private ShiftSorter() {}
+
+    private static final ShiftSorter INSTANCE = new ShiftSorter();
+
+    public static ShiftSorter getInstance() {
+        return INSTANCE;
+    }
+
+    public void sort(double[] array) {
         for (int i = 1; i < array.length; i++) {
             double a = array[i];
             int j = 0;
@@ -136,6 +86,76 @@ class ShiftSortableArray extends SortableArray {
             for (int k = i; k > j; k--)
                 array[k] = array[k-1];
             array[j] = a;
+        }
+    }
+}
+
+class SorterFactory {
+    public static Sorter getInstance(String type) throws Exception {
+        Sorter sorter = null;
+        switch (type.toLowerCase()) {
+            case "quick":
+                sorter = QuickSorter.getInstance();
+                break;
+            case "bubble":
+                sorter = BubbleSorter.getInstance();
+                break;
+            case "shift":
+                sorter = ShiftSorter.getInstance();
+                break;
+            default:
+                throw new Exception("Unresolved sort type: " + type);
+        }
+        return sorter;
+    }
+}
+
+interface DataReader {
+    double[] read() throws IOException;
+}
+
+class FileReader implements DataReader {
+    private final File file;
+
+    public FileReader(File file) {
+        this.file = file;
+    }
+
+    public double[] read() throws IOException {
+        try (Scanner in = new Scanner(file)) {
+            List<Double> list = new ArrayList<>();
+
+            while (in.hasNextDouble())
+                list.add(in.nextDouble());
+
+            double[] array = new double[list.size()];
+            for (int i = 0; i < array.length; i++)
+                array[i] = list.get(i);
+
+            return array;
+        }
+    }
+}
+
+public class Task3 {
+    public static void main(String[] args) {
+        try {
+            if (args.length != 2)
+                throw new IOException("Usage: [sort type] [input file name]");
+            String sortType = args[0];
+            File file = new File(args[1]);
+
+            DataReader reader = new FileReader(file);
+            double[] array = reader.read();
+
+            Sorter sorter = SorterFactory.getInstance(sortType);
+            sorter.sort(array);
+
+            System.out.println(Arrays.toString(array));
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace(System.err);
         }
     }
 }
