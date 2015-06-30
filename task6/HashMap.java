0a62f0aa561f411d4766e849d154991e537a7963
kelxvel <kelxvel@gmail.com>
2015-07-01 00:24:16 +0300

    Implemented resize and entrySet

--- task6/HashMap.java
+++ task6/HashMap.java
@@ -1,21 +1,32 @@
 import java.util.*;
+import java.util.Map.Entry;
 
+/**
+ * Created by kelxvel on 14.06.15.
+ */
 public class HashMap<K, V> {
     public static final int DEFAULT_INITIAL_CAPACITY = 16;
     public static final int MAXIMUM_CAPACITY = 1073741824;
     public static final float DEFAULT_LOAD_FACTOR = 0.75F;
 
-    private Map.Entry<K, V>[] table;
-    private int size;
+    private Entry<K, V>[] table;
 
-    private final int threshold;
+    private int size;
+    private int threshold;
     private final float loadFactor;
 
     public HashMap(int capacity, float loadFactor) {
+        if (capacity < 0)
+            throw new IllegalArgumentException("Illegal initial capacity: " + capacity);
+        if (capacity > MAXIMUM_CAPACITY)
+            capacity = MAXIMUM_CAPACITY;
+        if ((loadFactor <= 0.0F) || Float.isNaN(loadFactor))
+            throw new IllegalArgumentException("Illegal load factor: " + loadFactor);
+
         this.size = 0;
         this.table = new HashEntry[capacity];
         this.loadFactor = loadFactor;
-        this.threshold = (int) (table.length * loadFactor);
+        this.threshold = (int) (capacity * loadFactor);
     }
 
     public HashMap(int capacity) {
@@ -26,21 +37,23 @@ public class HashMap<K, V> {
         this(DEFAULT_INITIAL_CAPACITY, DEFAULT_LOAD_FACTOR);
     }
 
-    public void add(K key, V value) {
+    public V put(K key, V value) {
         int h = hash1(key);
         for (int i = 0; i < table.length; i++) {
             int idx = indexFor(h, table.length);
             if (table[idx] == null) {
                 table[idx] = new HashEntry<>(key, value);
-                return;
-            }
-            else if (key.equals(table[idx].getKey())) {
+                break;
+            } else if (key.equals(table[idx].getKey())) {
+                V oldValue = table[idx].getValue();
                 table[idx].setValue(value);
-                return;
+                return oldValue;
             }
             h += i * hash2(key);
         }
-        //resize();
+        if (++size > threshold)
+            resize();
+        return null;
     }
 
     public V get(Object key) {
@@ -69,17 +82,55 @@ public class HashMap<K, V> {
         return false;
     }
 
+    public Set<Entry<K, V>> entrySet() {
+        Set<Entry<K, V>> set = new HashSet<>(size);
+        for (int i = 0; i < table.length; i++)
+            if (table[i] != null)
+                set.add(table[i]);
+        return set;
+    }
+
+    private void resize() {
+        if (table.length >= MAXIMUM_CAPACITY) {
+            threshold = Integer.MAX_VALUE;
+            return;
+        }
+
+        int newCapacity = table.length * 2;
+        threshold = (int) (newCapacity * loadFactor);
+        Entry<K, V>[] oldTable = table;
+        Entry<K, V>[] newTable = new Entry[newCapacity];
+        table = newTable;
+
+        for (int i = 0; i < oldTable.length; i++) {
+            if (oldTable[i] == null)
+                continue;
+            K key = oldTable[i].getKey();
+            V value = oldTable[i].getValue();
+
+            int h = hash1(key);
+            for (int j = 0; j < table.length; j++) {
+                int idx = indexFor(h, table.length);
+                if (table[idx] == null) {
+                    table[idx] = new HashEntry<>(key, value);
+                    break;
+                }
+                h += j * hash2(key);
+            }
+            oldTable[i] = null;
+        }
+    }
 
     private static int hash1(Object key) {
-        return key.hashCode() % 13;
+        return key.hashCode() * 31;
     }
 
     private static int hash2(Object key) {
-        return 1 + key.hashCode() % 11;
+        return 1 + key.hashCode() * 63;
     }
 
     private static int indexFor(int h, int length) {
-        return h & (length - 1);
+        return h % length;
     }
 
     private static class HashEntry<K, V> implements Map.Entry<K, V> {
@@ -103,18 +154,20 @@ public class HashMap<K, V> {
     }
 
     public static void main(String[] args) {
-        HashMap<Integer, String> hm = new HashMap<>();
+        HashMap<Integer, String> hm = new HashMap<>(4);
 
         for (int i = 0; i < 10; i++) {
-            hm.add(i, "H: " + i);
+            hm.put(i, "i: " + i);
         }
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println(hm.get(i));
+        for (Entry<Integer, String> e : hm.entrySet()) {
+            //if (e == null) continue;
+            System.out.println(e.getValue());
         }
 
         hm.remove(4);
 
+        System.out.println("after remove/");
         for (int i = 0; i < 10; i++) {
             System.out.println(hm.get(i));
         }
