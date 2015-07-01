c404c06253f968a1a44dbe7b38d56c76b9579e8a
kelxvel <kelxvel@gmail.com>
2015-07-01 21:53:11 +0300

    Initial commit

---
+++ task7/ValidatorHTML.java
@@ -0,0 +1,42 @@
+import java.util.Stack;
+import java.util.regex.*;
+
+public class ValidatorHTML {
+    public boolean validate(String htmlCode) {
+        Pattern p = Pattern.compile("\\<(.*?)\\>");
+        Matcher m = p.matcher(htmlCode);
+
+        Stack<String> elements = new Stack<>();
+        while (m.find()) {
+            String tag = m.group();
+            if (tag.startsWith("</")) {
+                String element = tag.replaceFirst("</", "").replaceAll(">$", "");
+                if (!elements.empty()) {
+                    String e = elements.pop();
+                    if (!e.equalsIgnoreCase(element)) {
+                        System.out.println("Error in column " + htmlCode.indexOf(tag) + ": " + tag);
+                        return false;
+                    }
+                } else {
+                    System.out.println("Error in column " + htmlCode.indexOf(tag) + ": " + tag);
+                    return false;
+                }
+            } else if (tag.endsWith("/>")) {
+                continue;
+            } else {
+                tag = tag.replaceFirst("<", "").replaceAll(">$", "");
+                String element = (tag.split(" ", 2))[0]; // get only element-name without params
+                elements.push(element);
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String html = "<html><body><p align=“right”><strong>Hello world!</strong></p></body></html>";
+        String html2 = "<html><body><p align=“right”><strong>Hello world!</p></body></html>";
+        ValidatorHTML validator = new ValidatorHTML();
+        System.out.println(validator.validate(html));
+        System.out.println(validator.validate(html2));
+    }
+}
